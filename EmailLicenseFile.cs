74fc38ad776235d5622c986bfbd90c52ea2505dc
M.C.S.D. Jorge Apodaca <japodaca@bts.com.mx>
2019-05-24 20:33:39 -0700

    Last changes

---
+++ EmailLicenseFile.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using SendGrid.Helpers.Mail;
+
+namespace pluralsightfuncs
+{
+    public static class EmailLicenseFile
+    {
+        [FunctionName("EmailLicenseFile")]
+        public static void Run(
+			[BlobTrigger("licenses/{name}", Connection = "AzureWebJobsStorage")] string licenseFileContents,
+			[SendGrid(ApiKey = "SendGridApiKey")] out SendGridMessage message,
+			string name,
+			ILogger log)
+        {
+			var email = Regex.Match(input: licenseFileContents,
+				pattern: @"^Email\:\ (.+)$", RegexOptions.Multiline).Groups[1].Value;
+			log.LogInformation(message: $"Got order from {email}\n License file Name:{name}");
+
+			message = new SendGridMessage();
+			message.From = new EmailAddress(Environment.GetEnvironmentVariable("EmailSender"));
+			message.AddTo(email.Replace("\r",""));
+			var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(licenseFileContents);
+			var base64 = Convert.ToBase64String(plainTextBytes);
+			message.AddAttachment(name, base64, "text/plain");
+			message.Subject = "Your license file";
+			message.HtmlContent = "Thank you for your order";
+        }
+    }
+}
